3182e1f126e00c21568b31a9c499e1e068efcd9d
Chris Fredricksen <cmfredricksen@gmail.com>
2022-07-11 13:04:44 -0500

    Initial Commit

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	kit: {
+		adapter: adapter(),
+		lib: "src/lib"
+	},
+
+	preprocess: [mdsvex(mdsvexConfig)]
+};
+
+export default config;
