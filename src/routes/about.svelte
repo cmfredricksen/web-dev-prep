abd669c4c5c6d8fcb14b5c598bbb63b2f0d0ab9b
Chris Fredricksen <cmfredricksen@gmail.com>
2022-07-12 22:21:06 -0500

    added about content

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,21 @@
 <svelte:head>
     <title>Web Dev Prep | About</title>
 </svelte:head>
+
+<h2>About Web Dev Prep</h2>
+
+<p>This website is based on my notes as I am learning web development. I have a lot of experience building websites using platforms like WordPress, Shopify, Wix and others. A few years ago I decided to learn how to code a website from scratch, starting with HTML on <a href="https://www.w3schools.com/html/">W3 Schools</a>. Once I got a little way into that I downloaded <a href="https://code.visualstudio.com/">VSCode</a> and set it up.</p>
+
+<p>Since that time I have been learning non-stop and I am surprised at the skills I have picked up and how much easier it is to work with good basic skills. VSCode is a powerful piece of software that is a joy to use, but it took me quite a while to learn how to really use it to full benefit.</p>
+
+<p>I am building this site as a place to share my notes for other beginners, and also as a quick reference for myself. One aspect of coding is looking things up. Well I often go look at my own code base on GitHub to find answers because it is easier than googling it and finding the solution. Not much, once you look something up Google remembers it for you. But it is often quicker to go straight to the answer in my own code.</p>
+
+<p>I also want to share the joy of these basic building blocks of coding. Terminal, VSCode, Vim, GitHub and etc. etc.</p>
+
+<h3>TIP: When you are struggling with a coding concept and ready to take a break, use that time to get better at basic skills by learning or practicing them. Some time here and there will eventually add up!</h3>
+
+<style>
+    p {
+        margin: 1rem;
+    }
+</style>
