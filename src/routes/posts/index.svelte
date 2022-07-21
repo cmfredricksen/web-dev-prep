2be9d020b942e57a684ba76d547a38971baee6cc
Chris Fredricksen <cmfredricksen@gmail.com>
2022-07-21 02:09:33 -0500

    formatted posts/index

--- src/routes/posts/index.svelte
+++ src/routes/posts/index.svelte
@@ -33,18 +33,18 @@
 
 <ul>
     {#each dateSorted as { path, metadata: {title, tags, date}}}
-        <li>
-            <a class="title-link" href={`/posts/${path.replace(".md", "")}`}>{title}</a>
+    <li>
+    <a class="title-link" href={`/posts/${path.replace(".md", "")}`}>{title}</a>
             <small class="date">{ new Date(date).toDateString() }</small>
             <small>
                 <p>
                     tags: 
-                {#each tags as tag}
-                        <a class="tag" href={`/tags/${tag}`}> #{tag} </a>
-                        {/each}
+                    {#each tags as tag}
+                    <a class="tag" href={`/tags/${tag}`}> #{tag} </a>
+                    {/each}
                 </p>
             </small>
-        </li>
+    </li>
     {/each}
 </ul>
 
@@ -52,22 +52,38 @@
 <style>
     li {
         margin: .5rem 0;
+        padding: 1rem;
         display: flex;
         flex-direction: column;
+        font-weight: bold;
+    }
+
+    p, small {
+        text-decoration: none;
+    }
+    
+    li:nth-child(odd) {
+        background-color: var(--clr-black-blue-25);
     }
     .title-link  {
         margin: .75rem 0 .5rem;
         color: var(clr-black-blue);
+        text-decoration: underline;
     }
     .title-link:hover {
         color: var(--clr-accent);
     }
+    /* .date {
+        color: var(--clr-accent);
+    }
     .tag {
         text-decoration: none;
-        color: var(--clr-black-blue-lt);
-        font-weight: bold;
+        font-weight: normal;
+        color: var(--clr-grey-blue-xlt);
+        background-color: var(--clr-black-blue-50);
+        padding: 0 .25rem;
     }
     .tag:hover {
         color: var(--clr-accent);
-    }
+    } */
 </style>
